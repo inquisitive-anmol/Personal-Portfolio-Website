03131d8fec74cce3b594d5248b70f0476571a591
anmol <anmolof10gms@gmail.com>
2024-03-14 19:30:32 +0530

    modified the website

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a')
 const header = document.querySelector('.header.container');
+const brandh1 = document.querySelector('.brand h1');
+const menus_a = document.querySelectorAll('#header .nav-bar ul a');
 
 
 hamburger.addEventListener('click' , () => {
@@ -13,9 +15,19 @@ hamburger.addEventListener('click' , () => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if(scroll_position > 250) {
-        header.style.backgroundColor = '#29323c';
+        header.style.backgroundColor = '#b2c7d6';
+        brandh1.style.color = '#000';
+
+        menus_a.forEach(element => {
+            element.style.color = '#000';
+        });
     } else {
         header.style.backgroundColor = 'transparent';
+        brandh1.style.color = '#fff';
+
+        menus_a.forEach(element => {
+            element.style.color = '#fff';
+        });
     }
 });
 
